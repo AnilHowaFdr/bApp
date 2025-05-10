edfe61475b3f49582ee923aea6c983f037df64d5
Heron Mia <howa.anil@gmail.com>
2025-05-10 22:04:12 +0600

    app

---
+++ src/components/home/AppImage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const AppImage = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default AppImage;
--- src/components/home/HomeComponent.jsx
+++ src/components/home/HomeComponent.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import BkashApp from "./BkashApp";
+import AppImage from "./AppImage";
 
 const HomeComponent = () => {
   return (
     <section className="py-20">
       <div className="container mx-auto px-4">
         <BkashApp />
+        <AppImage />
       </div>
     </section>
   );
